54a8c337ca11ce544e9a66c262fdc676f99b1dc5
Brenda Rychter <brendarychter@gmail.com>
2017-08-20 22:12:42 -0300

    changes

--- admin/log_in.php
+++ admin/log_in.php
@@ -3,22 +3,28 @@
 	require_once("connection.php");
     $connection = new connection;
 
-	if(isset($_POST['username']) && isset($_POST['password'])){
-		$username = $_POST["username"];
-		$password = $_POST["password"];
-		$consulta = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
-		$response = mysqli_query($connection->connected, $consulta);
+    if(!mysqli_connect_error()){
+		if(isset($_POST['username']) && isset($_POST['password'])){
+			$username = $_POST["username"];
+			$password = $_POST["password"];
+			$consulta = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+			$response = mysqli_query($connection->connected, $consulta);
 
-		if($response = mysqli_query($connection->connected, $consulta)){
-			if(mysqli_num_rows($response)>=1){
-				while($obj = mysqli_fetch_object($response)){
-					$matriz = array('username' => $obj->username, 'password' => $obj->password, 'mail' => utf8_encode($obj->mail), 'phoneNumber' => $obj->phoneNumber, 'userID' => $obj->userID, 'active' => $obj->active);
+			if($response = mysqli_query($connection->connected, $consulta)){
+				if(mysqli_num_rows($response)>=1){
+					while($obj = mysqli_fetch_object($response)){
+						$matriz = array('username' => $obj->username, 'password' => $obj->password, 'mail' => utf8_encode($obj->mail), 'phoneNumber' => $obj->phoneNumber, 'userID' => $obj->userID, 'active' => $obj->active);
+					}
+				}else{
+	        		$matriz = array('userID'=>0);
 				}
-			}else{
-        		$matriz = array('userID'=>0);
 			}
+			$datos = json_encode($matriz);
+			echo $datos;
+		}else{
+			echo "data not setted";
 		}
-		$datos = json_encode($matriz);
-		echo $datos;
+	}else{
+		echo "error en conexion";
 	}
 ?>
