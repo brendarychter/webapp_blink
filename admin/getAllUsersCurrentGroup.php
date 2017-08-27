118d4cd6b502021120eafb254097efec64128b53
Brenda Rychter <brendarychter@gmail.com>
2017-08-27 18:19:05 -0300

    changes

---
+++ admin/getAllUsersCurrentGroup.php
@@ -0,0 +1,20 @@
+<?php
+	require_once("connection.php");
+	header("Access-Control-Allow-Origin: *");
+    $connection = new connection;
+
+    if(!mysqli_connect_error()){
+    	$idGroup = $_POST["idGroup"];
+
+		$query = "select * from modules inner join users on users.userID = modules.idUser where modules.idGroup = '$idGroup'";
+		$response = mysqli_query($connection->connected, $query);
+
+		while($obj = mysqli_fetch_object($response)){
+			$matriz[] = array('username' => $obj->username, 'phoneNumber' => $obj->phoneNumber, 'photo' => $obj->photo);
+		}
+		$datos = json_encode($matriz);
+		echo $datos;
+	}else{
+		echo "error en conexion";
+	}
+?>
