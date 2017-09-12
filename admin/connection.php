ba9b66cde7f5d3ccf8a473dbcb9f315238ddd04e
Brenda Rychter <brendarychter@gmail.com>
2017-09-11 23:12:06 -0300

    fix

--- admin/connection.php
+++ admin/connection.php
@@ -1,16 +1,16 @@
 <?php 
 	class connection{
-		private $server = "localhost";
-		private $user = "root";
-		private $pass = "";
-		private $bbdd = "blink";
-		public $connected;
-
 		// private $server = "localhost";
-		// private $user = "m6000758_blink";
-		// private $pass = "lu43seGIza";
-		// private $bbdd = "m6000758_blink";
+		// private $user = "root";
+		// private $pass = "";
+		// private $bbdd = "blink";
 		// public $connected;
+
+		private $server = "localhost";
+		private $user = "m6000758_blink";
+		private $pass = "lu43seGIza";
+		private $bbdd = "m6000758_blink";
+		public $connected;
 		
 		function __construct(){
 			$this->connected = @mysqli_connect($this->server, $this->user, $this->pass, $this->bbdd);
